0b2e94f18458c3e0bf9361a9e7847706e73caca3
HussienAboSoker <128934263+HussienAboSoker@users.noreply.github.com>
2026-05-05 12:58:34 +0300

    Handle specific exceptions based on the type of DioException

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,26 +1,40 @@
-
 import 'package:dio/dio.dart';
 import 'package:jawy/models/wheather_model.dart';
 
 class WeatherService {
-   final dio ;
-  WeatherService({required this.dio});
+  final _dio;
+  WeatherService(this._dio);
   Future<WeatherModel> getWeather({required String city}) async {
-   
-    const baseApi = 'https://api.weatherapi.com/v1/forecast.json?';
-    const apiKey = 'key=e30d28c544c74872b25135153232505';
+    const String baseApi = 'https://api.weatherapi.com/v1/forecast.json?';
+    const String apiKey = 'key=e30d28c544c74872b25135153232505';
     final url = '$baseApi$apiKey&q=$city&days=10';
 
     try {
-  final Response response = await dio.get(url);
-     
-    return WeatherModel.fromJson(response.data);
-} on DioException catch (e) {
-  String message = e.response?.data["error"]["message"] ?? "An error occurred"  ;
-  throw Exception(message);
-} catch (e) {
-  throw Exception("An unexpected error occurred: $e");
-}
-   
+      final Response response = await _dio.get(url);
+
+      return WeatherModel.fromJson(response.data);
+    } on DioException catch (e) {
+      // Handle specific exceptions based on the type of DioException
+      if (e.type == DioExceptionType.connectionTimeout ||
+          e.type == DioExceptionType.receiveTimeout ||
+          e.type == DioExceptionType.sendTimeout) {
+        throw Exception("Connection timed out. Please try again later.");
+        //
+      } else if (e.type == DioExceptionType.badCertificate) {
+        throw Exception(
+            "Invalid SSL certificate. Please check your connection.");
+      } else if (e.type == DioExceptionType.unknown) {
+        throw Exception(
+            "No internet connection. Please check your network settings.");
+      } else if (e.type == DioExceptionType.badResponse) {
+        throw Exception(
+            "Received invalid response from the server. Please try again later.");
+      }
+      String message =
+          e.response?.data["error"]["message"] ?? "An error occurred";
+      throw Exception(message);
+    } catch (e) {
+      throw Exception("An unexpected error occurred: $e");
+    }
   }
 }
